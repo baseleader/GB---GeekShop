14fdd2ec860d4e7dbed777960e5729b712b72385
baseleader <vashukstr8@mail.ru>
2021-09-25 17:03:27 +0300

    add paginator

--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -14,13 +14,15 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, id=None):
-    context = {'title': 'Каталог',
-               'category': ProductsCategory.objects.all(),
-               }
-    if id == 7:
-        products_filter = Product.objects.all()
-        context['products'] = products_filter
-    else:
-        context.update({'products': Product.objects.filter(category_id=id) if id != None else Product.objects.all()})
+def products(request, id=None, page=1):
+    products = Product.objects.filter(category_id=id) if id != None else Product.objects.all()
+    paginator = Paginator(products, per_page=3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+
+    context = {'title': 'Каталог', 'category': ProductsCategory.objects.all(), 'products': products_paginator}
     return render(request, 'products/products.html', context)
