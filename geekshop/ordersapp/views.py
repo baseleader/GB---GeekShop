4d8a81956f1c73d69be07c84bcab9dc49ec73a34
baseleader <vashukstr8@mail.ru>
2021-10-05 16:27:57 +0300

    added lesson3

---
+++ geekshop/ordersapp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+
+
+class OrderList(ListView):
+    pass
+
+
+class OrderCreate(CreateView):
+    pass
+
+
+class OrderUpdate(UpdateView):
+    pass
+
+
+class OrderDelete(DeleteView):
+    pass
+
+
+class OrderRead(DetailView):
+    pass
+
+
+class OrderItemsCreate(CreateView):
+    pass
+
+
+def order_forming_complete(request, pk):
+    pass
